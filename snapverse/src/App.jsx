b0da9f41796ec15f2eb148a46aa66dd4d5d46e7d
Adarsh Kumar <adarshkumar12103@gmail.com>
2025-11-29 18:18:08 +0530

    t

--- snapverse/src/App.jsx
+++ snapverse/src/App.jsx
@@ -4,24 +4,21 @@ import Navbar from "./assets/components/Navbar";
 const App = () => {
   return (
     <div className="min-h-screen bg-stone-500">
-
       {/* Navbar */}
       <Navbar />
 
-      {/* Main Content */}
-      <div className="text-white text-7xl font-bold p-10">
-        SHARE YOUR 
+      {/* Main Content with Tailwind animations */}
+      <div className="text-white font-bold p-10">
+        <div className="text-7xl animate-bounce">
+          SHARE YOUR
+        </div>
+        <div className="text-7xl animate-pulse">
+          PHOTOS
+        </div>
+        <div className="text-6xl animate-fadein">
+          WITH WORLD
+        </div>
       </div>
-
-      <Text />
-    </div>
-  );
-};
-
-const Text = () => {
-  return (
-    <div className="text-white text-3xl font-bold p-6">
-      PHOTOS 
     </div>
   );
 };
